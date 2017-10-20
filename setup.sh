f592e9e4e2ce4758915546fc5281cc48ba8ed9b5
Tomasz Klim <github@tomaszklim.pl>
2017-10-20 13:52:07 +0200

    new sf-repos extension

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/farm/scripts/setup/role.sh sf-farm-manager
+/opt/farm/scripts/setup/extension.sh sf-farm-manager
 
 ln -sf /opt/farm/ext/rsync-manager/add-rsync-user.sh /usr/local/bin/add-rsync-user
