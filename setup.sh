de90a461b28efb70e4ac93dc988d03c157c81b7a
Tomasz Klim <github@tomaszklim.pl>
2020-01-29 23:23:45 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@
 /opt/farm/scripts/setup/extension.sh sf-net-utils
 /opt/farm/scripts/setup/extension.sh sf-passwd-utils
 /opt/farm/scripts/setup/extension.sh sf-farm-manager
-
-ln -sf /opt/farm/ext/rsync-manager/add-rsync-user.sh /usr/local/bin/add-rsync-user
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/add-rsync-user ]; then
-	rm -f /usr/local/bin/add-rsync-user
-fi
